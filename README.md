629c9f0c01535d6ab6c274c0fca647530b2d08d9
Frank Luongo <frank.luongo@revzilla.com>
2019-04-08 14:12:08 -0400

    useState

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 LevelUp Tutorials
 
 ## useState
-```react
 
+React Hooks in Action for useState
+```jsx
+import React, { useState } from 'react';
+const [ name, setName ] = useState('');
+<input type="text" onChange={(e) => setName(e.target.value)} value={name}/>
 ```
+- Import `useState`
+- Create a variable with useState that sets up what you want to put in it
+- Add a way to interact with state on your component
--- src/Application.js
+++ src/Application.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Application = () => {
+
+  /*
+    Expected Format
+    const [ value, setValue ] = useState(initialStateValue);
+  */
+  const [ name, setName ] = useState('');
+
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <input type="text" onChange={(e) => setName(e.target.value)} value={name}/>
     </div>
   );
 };
