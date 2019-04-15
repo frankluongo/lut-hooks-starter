2d19017220470cd603a012c375ba2e4ba14c8c60
Frank Luongo <frank.luongo@revzilla.com>
2019-04-15 08:37:01 -0400

    Advanced State Management With useReducer

--- src/Application.js
+++ src/Application.js
@@ -3,6 +3,7 @@ import Toggle from './utils/Toggle';
 import { formSubmit } from './utils/Forms';
 import { useTitleInput } from './utils/useTitleInput';
 import UserContext from './contexts/UserContext';
+import Counter from './components/Counter';
 
 const Application = () => {
   const [ name, setName ] = useTitleInput('');
@@ -17,6 +18,7 @@ const Application = () => {
       <main className="main-wrapper" ref={ref}>
         <h1>Level Up Dishes</h1>
         <Toggle>Toggle Content</Toggle>
+        <Counter />
         <form onSubmit={e => {e.preventDefault();formSubmit(name, setName);}}>
           <input type="text" onChange={(e) => setName(e.target.value)} value={name}/>
           <input type="submit" value="submit"/>
